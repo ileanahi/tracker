afa8846de8f051a5c6d24635819d4ca8b0bbd82b
Ileana Argyris <argyris.ileana@gmail.com>
2020-11-22 04:44:07 +0200

    Initial Commit

---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+const Loader = () => {
+    return (
+        <div className="loader">
+            <div id="preloader">
+                <div id="loader"></div>
+            </div>
+            <h1>Fetching Data</h1>
+        </div>
+    )
+}
+
+export default Loader
